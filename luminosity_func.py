9302ed4278921229cf4f17c36ffdd792521b83a0
Lorenzo Cane <lorenzo@lorenzo.laptop>
2024-03-18 17:34:24 +0100

    power law modif+ln

--- luminosity_func.py
+++ luminosity_func.py
@@ -14,7 +14,7 @@ min = 1.0e29      #erg s^(-1)     #min for graph
 max = 1.0e38       #erg s^(-1)     #max for graph
 r = 1.11e-46       #cm^(-2)        #flux/lum ratio
 
-l= np.linspace(min, max,10000)     #luminosity
+l= np.geomspace(min, max,10000)     #luminosity
 f_pl = l * r                       #flux
 
 #power law - wavelet 1
@@ -24,22 +24,28 @@ alpha1 = 1.94  #slope
 
 
 
-p_pl1 = l**(-alpha1) * np.exp(-l/l_M1) / (gamma((l_m1/l_M1)) * l_M1**(1.0-alpha1)) #power law lum funcs
+p_pl1 = l**(-alpha1) * np.exp(-l/l_M1) / (gamma((l_m1/l_M1)) * l_M1**(1.0-alpha1)) #calore,cholis,weniger power law lum funcs
 
 #-----------------------------------------------------------------------------
 #power law - wavelet 2
 l_m2 = 1.0e29  #erg s^(-1)  #L_min : low flux step-func cutoff
 l_M2 = 7.0e34  #erg s^(-1)   #L_MAX : high flux exp cutoff
-alpha2 = 1.95  #slope
+alpha2 = 1.5  #slope
 
-p_pl2 = l**(-alpha2)  *np.exp(-l/l_M2) / (gamma((l_m2/l_M2)) * l_M2**(1.0-alpha2)) #power law lum funcs
+p_pl2 = l**(-alpha2)  *np.exp(-l/l_M2) / (gamma((l_m2/l_M2)) * l_M2**(1.0-alpha2)) #bartles power law lum funcs
 
+#-----------------------------------------------------------------------------
+#GLC - log normal
+l_0_glc = 8.8e33
+sigma_glc = 0.62 
+
+p_glc = np.log10(np.e) / (sigma_glc * (2*np.pi)**(0.5) * l) * np.exp((-1.0* (np.log10(l) - np.log10(l_0_glc))**2)/2/sigma_glc/sigma_glc)
 #****************************************************************************
 #PLOTS
 
 #power law
 
-fig, ax1 = plt.subplots(figsize= (16,9))
+fig, ax1 = plt.subplots()
 ax2 = ax1.twiny()
 
 ax1.set_yticks(np.linspace(1.0e-45, 1.e-29, 5))
@@ -53,6 +59,7 @@ ax1.set_ylabel("dN / dL")
 
 ax1.loglog(l,p_pl1, color = 'lightblue')
 ax1.loglog(l, p_pl2, color = 'green')
+ax1.loglog(l, p_glc)
 ax2.loglog(f_pl, p_pl1, color = 'lightblue')
 ax2.loglog(f_pl, p_pl2, color = 'green')
 
@@ -64,7 +71,7 @@ plt.savefig(os.path.join('pl.png'))
 #----------------------------------------------------------------------------
 #e-fold
 
-fig2 , ax3 = plt.subplots(figsize = (15,15))
+fig2 , ax3 = plt.subplots()
 ax4 = ax3.twiny()
 
 ax3.set_yticks(np.geomspace(1.0e-7, 1.0e2, 4))
@@ -75,8 +82,9 @@ ax3.set_xlabel("Luminosity L [erg / s]")
 ax4.set_xlabel(r'Flux F [erg / $\mathregular{cm^2}$ / s]')
 ax3.set_ylabel("LdN / dL")
 
-ax3.loglog(l,l * p_pl1)
-ax3.loglog(l, l * p_pl2)
+ax3.loglog(l,l * p_pl1, color = 'lightblue')
+ax3.loglog(l, l * p_pl2, color = 'green')
+ax3.loglog(l, l*p_glc)
 #ax4.loglog(f_pl,l * p_pl1)
 
 fig2.tight_layout()
