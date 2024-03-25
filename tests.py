3a8e68417e283aaccfc2c66519cc597fb04c780b
Lorenzo Cane <lorenzo@lorenzo.laptop>
2024-03-25 17:10:07 +0100

    testing integrals

--- tests.py
+++ tests.py
@@ -2,21 +2,33 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.special import gammainc
 import scipy.integrate as integrate
 import os
 
 
-#def gNFW(r, gamma, r_s):
- #   return (r/r_s)**(-gamma) * (1 + r/r_s)**(gamma-3)
+#def gNRW(s, rs, gamma, rc):
+#    r = np.sqrt(s*s + rc*rc - 2*s*rc)
+#    a = (r / rs)**(-gamma)
+#    b = (1 + r / rs)**(-3+gamma)
 
+#    return a * b 
 
-#Flux = integrate.quad()
-    
-def integrand(t, a):
-    return t**(a-1) * np.exp(-t)
+#l = 20
+#b = 20
+#rs = 20
+#gamma = 1.2
+#rc = 8.5
 
-def gamma_inc(s,x):
-    return integrate.quad(integrand, 1.0e-6 ,1000000000,  args=(s))
+#I = integrate.quad(gNRW, 1.0e-8 , 1.0e8, args=(rs, gamma, rc))
+b_min = np.deg2rad(2)
+b_max = np.deg2rad(20)
+l_min = 0
+l_max = b_max
 
-print(gamma_inc(-0.94, 1))
+ #I = integrate.dblquad(lambda l, b : 4*np.cos(l)*np.cos(b), 0, l_max)
+def int(x, b):
+    return np.cos(x)*b
+
+i1 = integrate.quad(int, l_min , l_max, args=(1))[0]
+i2 = integrate.quad(int, b_min , b_max, args=(1))[0]
+print(4*i1*i2)
