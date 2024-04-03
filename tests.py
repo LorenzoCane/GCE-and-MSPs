d07cf0716e1ee6fd20667a86c4903396c95c4182
Lorenzo Cane <lorenzo@lorenzo.laptop>
2024-04-03 16:50:43 +0200

    minor on gamma inc

--- tests.py
+++ tests.py
@@ -5,48 +5,70 @@ import matplotlib.pyplot as plt
 import scipy.integrate as integrate
 import os
 
+def cmtokpc(x):                  
+    #convertion of cm to kpc 
+    return x*3.2407792896664e-22 
 
-#def gNRW(s, rs, gamma, rc):
-#    r = np.sqrt(s*s + rc*rc - 2*s*rc)
-#    a = (r / rs)**(-gamma)
-#    b = (1 + r / rs)**(-3+gamma)
 
-#    return a * b 
+def gNRW2(s, l , b , rs, gamma, rc):    
+    #general Navarro-Frenk-White squared
+    #s: Earth point distance; l, b:  long and lat.; rs:scale radius; gamma: slope; rc:Earth-GC dist.
+    r = np.sqrt(s*s + rc*rc - 2*s*rc*np.cos(l)*np.cos(b))
+    a = (r / rs)**(-2*gamma)
+    b = (1 + r / rs)**(2*(-3+gamma))
 
-#l = 20
-#b = 20
-#rs = 20
-#gamma = 1.2
-#rc = 8.5
+    return a * b 
 
-#I = integrate.quad(gNRW, 1.0e-8 , 1.0e8, args=(rs, gamma, rc))
+def denom(s, l , b , rs, gamma, rc):
+    # return s^2 * gNFW function 
+    #ALERT: THE RESULT HAS [s]^2 AS UNIT
+    return (s**2)*gNRW2(s, l , b , rs, gamma, rc)
+
 b_min = np.deg2rad(2)
 b_max = np.deg2rad(20)
 l_min = 0
 l_max = b_max
+rs = 20 #kpc
+gamma = 1.2 #kpc
+rc = 8.5 #kpc
 
- #I = integrate.dblquad(lambda l, b : 4*np.cos(l)*np.cos(b), 0, l_max)
-def int(x, b):
-    return np.cos(x)*b
+I = integrate.nquad(gNRW2, [[1.0e-6 , np.infty], [l_min, l_max], [b_min, b_max]] , args=(rs, gamma, rc))
+I2 = integrate.nquad(denom, [[1.0e-6 , np.infty], [l_min, l_max], [b_min, b_max]] , args=(rs, gamma, rc))
 
-i1 = integrate.quad(int, l_min , l_max, args=(1))[0]
-i2 = integrate.quad(int, b_min , b_max, args=(1))[0]
+#print(I[0] )
+
+
+#def int(x, b):
+#    return np.cos(x)*b
+
+i1 = integrate.quad(np.cos, l_min , l_max)[0]
+i2 = integrate.quad(np.cos, b_min , b_max)[0]
 stupid =4*i1*i2 
+#print(stupid)
+#GeVtoerg = 0.00160218
 
-GeVtoerg = 0.00160218
 
-
-def broken_pl(x, norm, x_b, n1, n2):             #norm: normalization, x_b: broken point, n1: 1st part index, n2: 2nd part index               
-    bpl = []
+#def broken_pl(x, norm, x_b, n1, n2):             #norm: normalization, x_b: broken point, n1: 1st part index, n2: 2nd part index               
+#   bpl = []
 
     
-    if x < x_b:
-        frac = (x/x_b)**(2-n1)
-    else:
-        frac = (x/x_b)**(2-n2)
-        bpl.append(norm *frac)
-    
-    return norm * frac
+#    if x < x_b:
+#       frac = (x/x_b)**(2-n1)
+#    else:
+#        frac = (x/x_b)**(2-n2)
+#        bpl.append(norm *frac)
+#    
+#    return norm * frac
 
-I = integrate.quad(broken_pl, 0.1, 10, args=(1.0e-6, 2.06, 1.42, 2.63))
-print(I[0] * GeVtoerg )
+#I = integrate.quad(broken_pl, 0.1, 10, args=(1.0e-6, 2.06, 1.42, 2.63))
+#print(I[0] * GeVtoerg )
+
+def integr(t, s):
+    return t**(s-1) * np.exp(-t)
+
+def mygamma(s,x):
+    I = integrate.quad(integr, x , 1.0e50, args=(s))
+    return I
+
+norm = mygamma(-0.94, 1.0e-6)[0]
+print(norm * (1.0e35)**(1-1.94))
