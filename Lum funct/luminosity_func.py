abe9ed81c75f78a38d3c860187ea2ca9e290665b
Lorenzo Cane <lorenzo@lorenzo.laptop>
2024-04-10 13:01:27 +0200

    integration opts impl

--- Lum funct/luminosity_func.py
+++ Lum funct/luminosity_func.py
@@ -1,7 +1,8 @@
 
 
 #import
-
+import time 
+from datetime import timedelta
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.integrate as integrate
@@ -11,26 +12,40 @@ from gce import gNRW2, sgNRW, cmtokpc, power_law, log_norm, broken_pl_arr, broke
 
 #****************************************************************************
 #USEFUL VALUES AND DEF
+start_time = time.monotonic()
+
 f = open('lum_func.txt', 'w')      #output file
 f.write('luminosity_func.py results \n')
 f.write('************************************************* \n \n')
 
+#luminosity values
 min = 1.0e29       #erg s^(-1)     #luminosity min 
 max = 1.0e38       #erg s^(-1)     #luminosity max
+l_th = 1.0e34      #erg s^(-1)     # luminosity threshold
+#integration conditions
+abs_err = 0.0
+rel_err = 1.0e-6
+div_numb = 50
+#ROI bounds
 b_min = np.deg2rad(2)              #ROI latitude min value
 b_max = np.deg2rad(20)             #ROI latitude max value 
 l_min = 0                          #ROI long. min value
 l_max = b_max                      #ROI long. max value
+
+#astronomical constants
 rs = 20            #kpc            #scale radius (gNFW) 
 g = 1.2            #kpc            #gamma-sloper (gNFW) 
 rc = 8.5           #kpc            #Earth-GC distance
 f_obs = 1.8e-9     #erg s^(-1)     #observed flux 
 
-#integration over ROI
-num = 4*integrate.nquad(gNRW2, [[1.0e-6 , np.infty], [l_min, l_max], [b_min, b_max]] , args=(rs, g, rc))[0]
-den = 4* integrate.nquad(sgNRW, [[1.0e-6 , np.infty], [l_min, l_max], [b_min, b_max]] , args=(rs, g, rc))[0]
+#Flux/Lum ratio
+options = {'epsabs': abs_err, 'epsrel' : rel_err}
+num = 4*integrate.nquad(gNRW2, [[1.0e-6 , np.infty], [l_min, l_max], [b_min, b_max]] , args=(rs, g, rc), opts=options)
+den = 4* integrate.nquad(sgNRW, [[1.0e-6 , np.infty], [l_min, l_max], [b_min, b_max]] , args=(rs, g, rc), opts = options)
+print(den[0], "  ", den[1])
+print(num[0], "  ", num[1])
 #!!! den is given in kpc^-2 !!!!
-r = cmtokpc(cmtokpc(num / den / 4 /np.pi))       #cm^(-2)        #flux/lum ratio
+r = cmtokpc(cmtokpc(num[0] / den[0] / 4 /np.pi))       #cm^(-2)        #flux/lum ratio
 
 line = ['Flux/Lum ratio: ', '\n','F/L = ', str(r), ' cm^(-2) \n']
 for l in line:
@@ -57,15 +72,15 @@ alpha1 = 1.94  #slope
 
 p_pl1 = power_law(l , alpha1 , l_m1 , l_M1, True) #ming power law lum func
 
-d = integrate.quad(power_law, 1.0e29, 1.0e38, args=(alpha1-1 , l_m1 , l_M1, True))[0]
-n = integrate.quad(power_law, 1.0e34, 1.0e38, args=(alpha1 , l_m1 , l_M1, True))[0]
-
+d = integrate.quad(power_law, min, max, args=(alpha1-1 , l_m1 , l_M1, True), epsabs = abs_err, epsrel = rel_err, limit=div_numb)
+n = integrate.quad(power_law, l_th, max, args=(alpha1 , l_m1 , l_M1, True), epsabs = abs_err, epsrel = rel_err, limit=div_numb)
+#print(d[0], "  ", d[1])
 f.write('\nWavelet 1: \n')
 f.write('N_GCE = ')
-f.write(str(f_obs / r / d))
+f.write(str(f_obs / r / d[0]))
 f.write('\n')
 f.write('N_r = ')
-f.write(str(f_obs / r / d*n))
+f.write(str(f_obs / r / d[0]*n[0]))
 
 f.write('\n')
 
@@ -77,15 +92,16 @@ alpha2 = 1.5  #slope
 
 p_pl2 = power_law(l, alpha2, l_m2, l_M2, False) #bartles power law lum func
 
-d = integrate.quad(power_law, 1.0e29, 1.0e38, args=(alpha2-1 , l_m2 , l_M2, False))[0]
-n = integrate.quad(power_law, 1.0e34, 1.0e38, args=(alpha2 , l_m2 , l_M2, False))[0]
+d = integrate.quad(power_law, min, max, args=(alpha2-1 , l_m2 , l_M2, False), epsabs = abs_err, epsrel = rel_err, limit=div_numb)
+n = integrate.quad(power_law, l_th, max, args=(alpha2 , l_m2 , l_M2, False), epsabs = abs_err, epsrel = rel_err, limit=div_numb)
+#print(d[0], "  ", d[1])
 
 f.write('\n\nWavelet 2: \n')
 f.write('N_GCE = ')
-f.write(str(f_obs / r / d))
+f.write(str(f_obs / r / d[0]))
 f.write('\n')
 f.write('N_r = ')
-f.write(str(f_obs / r / d*n))
+f.write(str(f_obs / r / d[0]*n[0]))
 
 
 
@@ -96,15 +112,16 @@ sigma_glc = 0.62
 
 p_glc = log_norm(l, l_0_glc, sigma_glc)  #log norm from global cluster obs
 
-d = integrate.quad(l_log, 1.0e29, 1.0e38, args=(l_0_glc, sigma_glc))[0]
-n = integrate.quad(log_norm, 1.0e34, 1.0e38, args=(l_0_glc, sigma_glc))[0]
+d = integrate.quad(l_log, min, max, args=(l_0_glc, sigma_glc), epsabs = abs_err, epsrel = rel_err, limit=div_numb)
+n = integrate.quad(log_norm, l_th, max, args=(l_0_glc, sigma_glc), epsabs = abs_err, epsrel = rel_err, limit=div_numb)
+#print(n[0], "  ", n[1])
 
 f.write('\n\nGLC: \n')
 f.write('N_GCE = ')
-f.write(str(f_obs / r / d))
+f.write(str(f_obs / r / d[0]))
 f.write('\n')
 f.write('N_r = ')
-f.write(str(f_obs / r / d*n))
+f.write(str(f_obs / r / d[0]*n[0]))
 
 #-----------------------------------------------------------------------------
 #GCE - log normal
@@ -113,15 +130,16 @@ sigma_gce = 0.70
 
 p_gce = log_norm(l, l_0_gce, sigma_gce)  #log norm from bulge obs
 
-d = integrate.quad(l_log, 1.0e29, 1.0e38, args=(l_0_gce, sigma_gce))[0]
-n = integrate.quad(log_norm, 1.0e34, 1.0e38, args=(l_0_gce, sigma_gce))[0]
+d = integrate.quad(l_log, min, max, args=(l_0_gce, sigma_gce), epsabs = abs_err, epsrel = rel_err, limit=div_numb)
+n = integrate.quad(log_norm, l_th, max, args=(l_0_gce, sigma_gce), epsabs = abs_err, epsrel = rel_err, limit=div_numb)
+#print(n[0], "  ", n[1])
 
 f.write('\n\nGCE: \n')
 f.write('N_GCE = ')
-f.write(str(f_obs / r / d))
+f.write(str(f_obs / r / d[0]))
 f.write('\n')
 f.write('N_r = ')
-f.write(str(f_obs / r / d*n))
+f.write(str(f_obs / r / d[0]*n[0]))
 
 #-----------------------------------------------------------------------------
 #AIC - log normal
@@ -130,15 +148,48 @@ sigma_aic = 0.94
 
 p_aic = log_norm(l, l_0_aic, sigma_aic)  #log norm from bulge obs
 
-d = integrate.quad(l_log, 1.0e29, 1.0e38, args=(l_0_aic, sigma_aic))[0]
-n = integrate.quad(log_norm, 1.0e34, 1.0e38, args=(l_0_aic, sigma_aic))[0]
+d = integrate.quad(l_log, min, max, args=(l_0_aic, sigma_aic), epsabs = abs_err, epsrel = rel_err, limit=div_numb)
+n = integrate.quad(log_norm, l_th, max, args=(l_0_aic, sigma_aic), epsabs = abs_err, epsrel = rel_err, limit=div_numb)
+#print(n[0], "  ", n[1])
 
 f.write('\n\nAIC: \n')
 f.write('N_GCE = ')
-f.write(str(f_obs / r / d))
+f.write(str(f_obs / r / d[0]))
 f.write('\n')
 f.write('N_r = ')
-f.write(str(f_obs / r / d*n))
+f.write(str(f_obs / r / d[0]*n[0]))
+
+#-----------------------------------------------------------------------------
+#NPTF - broken power law
+n1_nptf = -0.66
+n2_nptf = 18.2
+l_b_nptf = 2.5e34
+norm_nptf= (1-n1_nptf)*(1-n2_nptf) / l_b_nptf / (n1_nptf - n2_nptf)
+p_nptf = []
+
+p_nptf = broken_pl_arr(l, norm_nptf, l_b_nptf, n1_nptf, n2_nptf)
+
+#Search of integral upper bound
+#int_max = 1.0e34
+#contr = 1
+#while contr > 1.0e-50:
+#    int_max *= 2
+#    contr = broken_pl(int_max, norm_nptf, l_b_nptf, n1_nptf, n2_nptf)
+#
+#print(int_max, "   ", contr)
+
+int_max =3.2e35 #int upper bound as found before
+
+d = integrate.quad(l_bpl, min, int_max, args=(norm_nptf, l_b_nptf, n1_nptf, n2_nptf), epsabs = abs_err, epsrel = rel_err, limit=div_numb)
+n = integrate.quad(broken_pl, l_th, int_max, args=(norm_nptf, l_b_nptf, n1_nptf, n2_nptf), epsabs = abs_err, epsrel = rel_err, limit=div_numb)
+#print(n[0], "  ", n[1])
+
+f.write('\n\nNPTF: \n')
+f.write('N_GCE = ')
+f.write(str(f_obs / r / d[0]))
+f.write('\n')
+f.write('N_r = ')
+f.write(str(f_obs / r / d[0]*n[0]))
 
 #-----------------------------------------------------------------------------
 #Disk - broken power law
@@ -156,43 +207,18 @@ p_disk = broken_pl_arr(l, norm_disk, l_b_disk, n1_disk, n2_disk)
 
 norm_disk_lim = (1-n1_disk)*(1-n2_disk) / l_b_disk / a
 
-
-d = integrate.quad(l_bpl, l_m_disk, l_M_disk, args=(norm_disk_lim, l_b_disk, n1_disk, n2_disk))[0]
-n = integrate.quad(broken_pl, 1.0e34, l_M_disk, args=(norm_disk_lim, l_b_disk, n1_disk, n2_disk))[0]
+#num_range = np.geomspace(l_th, l_M_disk,10000)
+#den_range = np.geomspace(l_m_disk, l_M_disk, 10000)
+d = integrate.quad(l_bpl, l_m_disk, l_M_disk, args=(norm_disk_lim, l_b_disk, n1_disk, n2_disk), epsabs = abs_err, epsrel = rel_err, limit=div_numb)
+n = integrate.quad(broken_pl, l_th, l_M_disk, args=(norm_disk_lim, l_b_disk, n1_disk, n2_disk), epsabs = abs_err, epsrel = rel_err, limit=div_numb)
+#print(n[0], "  ", n[1])
 
 f.write('\n\nDisk: \n')
 f.write('N_GCE = ')
-f.write(str(f_obs / r / d))
+f.write(str(f_obs / r / d[0]))
 f.write('\n')
 f.write('N_r = ')
-f.write(str(f_obs / r / d*n))
-
-#-----------------------------------------------------------------------------
-#NPTF - broken power law
-n1_nptf = -0.66
-n2_nptf = 18.2
-l_b_nptf = 2.5e34
-norm_nptf= (1-n1_nptf)*(1-n2_nptf) / l_b_nptf / (n1_nptf - n2_nptf)
-p_nptf = []
-
-p_nptf = broken_pl_arr(l, norm_nptf, l_b_nptf, n1_nptf, n2_nptf)
-
-int_max = 1.0e34
-contr = 1
-while contr > 1.0e-50:
-    int_max *= 2
-    contr = broken_pl(int_max, norm_nptf, l_b_nptf, n1_nptf, n2_nptf)
-
-print(int_max, "   ", contr)
-d = integrate.quad(l_bpl, 1.0e29, int_max, args=(norm_nptf, l_b_nptf, n1_nptf, n2_nptf))[0]
-n = integrate.quad(broken_pl, 1.0e34, int_max, args=(norm_nptf, l_b_nptf, n1_nptf, n2_nptf))[0]
-
-f.write('\n\nNPTF: \n')
-f.write('N_GCE = ')
-f.write(str(f_obs / r / d))
-f.write('\n')
-f.write('N_r = ')
-f.write(str(f_obs / r / d*n))
+f.write(str(f_obs / r / d[0]*n[0]))
 
 #****************************************************************************
 #PLOTS
@@ -288,4 +314,7 @@ ax6.set_xlim(min*r, max*r)
 ax5.legend()
 plt.savefig(os.path.join('2efold.png'))
 
+end_time = time.monotonic()
 f.close()
+
+print(timedelta(seconds= end_time - start_time))
